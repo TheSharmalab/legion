7e4c1b5f6eb7f9e0ebb78936f9567ab15c7b5cb4
TheSharmalab <Nik@macintosh-2.local>
2017-09-17 06:52:07 +0100

    updates

---
+++ Legion_Freesurfer_recon_per_subject_1to5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# The ratio for this is 1 to 5. It will generate a recon all for 5 subjects per lines. Therefore 250 subjects is 50 (250/5) and then add on the jump required.
+for n in {1..50} ;do
+echo time recon-all" cut_${n}_mprage.nii.gz ;" time recon-all" cut_${n+50}_mprage.nii.gz ;" time recon-all" cut_${n+100}_mprage.nii.gz ;" time recon-all" cut_${n+150}_mprage.nii.gz ;" time recon-all" cut_${n+200}_mprage.nii.gz ;"  >> output.txt
+done
