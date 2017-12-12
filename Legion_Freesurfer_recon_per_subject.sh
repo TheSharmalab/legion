737ff6718cfdf8a0c2c6328201cb0f21d9b5741a
NS <nikhil.sharma@ucl.ac.uk>
2017-12-12 17:58:45 +0000

    updates

--- Legion_Freesurfer_recon_per_subject.sh
+++ Legion_Freesurfer_recon_per_subject.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+SUBJECTS_DIR=/scratch/scratch/rmjlns1/shared/freesurfer_subjects/subjects
 for n in {1..250} ;do
-echo "time recon-all -subject cut_${n} -all" >> output.txt
+echo "time recon-all -subject cut_${n} -all && echo cut_${n} "is processed" >> $SUBJECTS_DIR/catch_errors.txt || echo cut_${n} "has CRASHED" >> $SUBJECTS_DIR/catch_errors.txt
 done
 
--- Legion_Freesurfer_recon_per_subject_1to5.sh
+++ Legion_Freesurfer_recon_per_subject_1to5.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # The ratio for this is 1 to 5. It will generate a recon all for 5 subjects per lines. Therefore 250 subjects is 50 (250/5) and then add on the jump required.
+# The output file is $SUBJECTS_DIR/catch_errors.txt. 
+# The error catch is   "&& echo cut_${n} "is processed" >> $SUBJECTS_DIR/catch_errors.txt || echo cut_${n} "has CRASHED" >> $SUBJECTS_DIR/catch_errors.txt "
+
 for n in {1..50} ;do
-echo time recon-all -subject "cut_${n} -all ;"  time recon-all -subject "cut_$((n+50)) -all ;" time recon-all -subject "cut_$((n+100))-all ; " time recon-all -subject "cut_$((n+150)) -all ;" time recon-all -subject " cut_$((n+200)) -all ;"  >> output.txt
+echo time recon-all -subject "cut_${n} -all && echo cut_${n} "is processed" >> $SUBJECTS_DIR/catch_errors.txt || echo cut_${n} "has CRASHED" >> $SUBJECTS_DIR/catch_errors.txt ; "  time recon-all -subject "cut_$((n+50)) -all && echo cut_$((n+50)) "is processed" >> $SUBJECTS_DIR/catch_errors.txt || echo cut_$((n+50) "has CRASHED" >> $SUBJECTS_DIR/catch_errors.txt;" time recon-all -subject "cut_$((n+100))-all && echo cut_$((n+100)) "is processed" >> $SUBJECTS_DIR/catch_errors.txt || echo cut_$((n+100) "has CRASHED" >> $SUBJECTS_DIR/catch_errors.txt; " time recon-all -subject "cut_$((n+150)) -all && echo cut_$((n+150)) "is processed" >> $SUBJECTS_DIR/catch_errors.txt || echo cut_$((n+150) "has CRASHED" >> $SUBJECTS_DIR/catch_errors.txt ; " time recon-all -subject " cut_$((n+200)) -all && echo cut_$((n+200)) "is processed" >> $SUBJECTS_DIR/catch_errors.txt || echo cut_$((n+200))"has CRASHED" >> $SUBJECTS_DIR/catch_errors.txt ;"  >> output.txt
 done
 
 
